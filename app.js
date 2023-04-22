67a3e3553c7cc99b9df2f204a43e32397a3a4ea4
Alexander Royal <alexander.de.royal@gmail.com>
2023-04-22 15:40:05 -0500

    change name from application.js to app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+// Document: The document object is our entry point into the world of the DOM.
+// It contains representations of all the content on a page, plus tons of useful
+// methods and properties
+
