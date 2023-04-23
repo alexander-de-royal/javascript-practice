6914d4362cddd887e467ed02d0e87645246e438b
Alexander Royal <alexander.de.royal@gmail.com>
2023-04-22 19:34:28 -0500

    completed section-13

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 // It contains representations of all the content on a page, plus tons of useful
 // methods and properties
 
+document.getElementById('bear-photo')
+document.getElementsByTagName('input')
+const ul = document.getElementsByTagName('ul')[0]
+ul.getElementsByTagName('li')
+
+// Query Selector: is a newer all-in-one method to select a single element.
+document.querySelector('h1')
+document.querySelector('#bear-photo')
+document.querySelector('.special')
