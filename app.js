7556db924a9d3f11272424c8439e5145e6deeb37
Alexander Royal <alexander.de.royal@gmail.com>
2023-04-28 20:38:41 -0500

    completed section 8 from the web developer bootcamp

--- app.js
+++ app.js
@@ -179,4 +179,32 @@
 //     formData['cc'] = e.target.value;
 // });
 
+// Call Stack: The mechanism the JS interpreter uses to keep track of its place in a script that calls multiple functions.
+// How JS "knows" what function is currently being run and what functions are called within that function, etc.
+// const repeat = (str, times) => {
+//     let result = '';
+//     for (let i = 0; i < times; i++){
+//         result += str;
+//     }
+//     return result;
+// }
+// const scream = (str) => {
+//     return str.toUpperCase() + '!!!';
+// }
+//
+// const getRantText = (phrase) => {
+//     let text = scream(phrase);
+//     let rant = repeat(text, 8);
+//     return rant;
+// }
+//
+// const makeRant = (phrase, el) => {
+//     const h1 = document.createElement('h1');
+//     h1.innerText = getRantText(phrase);
+//     el.appendChild(h1);
+// }
+//
+// makeRant('I hate mayo', document.body);
+// makeRant('If you have water', document.body);
+
 
