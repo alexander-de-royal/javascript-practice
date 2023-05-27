44315dd6ff982a462b35796de1eadb056084bd57
Alexander Royal <alexander.de.royal@gmail.com>
2023-05-27 16:37:03 -0500

    weather api project  completed

--- weather-project/app.js
+++ weather-project/app.js
@@ -33,27 +33,35 @@
 // DISPLAY WEATHER IN ANY CITY
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function (req, res){
+app.use(bodyParser.urlencoded({extended: true}));
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=40cb7f55adbe6b3302d4780d0ead3739&units=metric"
+app.get("/", function (req, res){
+        res.sendFile(__dirname + "/index.html")
+});
+app.post("/", function (req, res){
+    const query = req.body.cityName;
+    const apiKey = "40cb7f55adbe6b3302d4780d0ead3739";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
     https.get(url, function (response){
-
         console.log(response.statusCode);
 
         response.on("data", function(data){
-           const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = "http://openweathermap.org/img/wn" + icon + "@2x.png"
-            res.write("<h1>The temperature in London is " + temp + " degree Celcius.</h1>");
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "https://openweathermap.org/img/wn" + icon + "@2x.png";
+
             res.write("The weather is currently " + weatherDescription);
+            res.write("The temperature in" + query + "is" + temp + "degree Celsius.");
             res.write("<img src=" + imageURL +  ">");
-            res.send()
+            res.send();
         });
     });
 });
