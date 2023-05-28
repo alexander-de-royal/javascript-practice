4dbc64f666c7b874fc7668b37d1fcfc5ed235739
Alexander Royal <alexander.de.royal@gmail.com>
2023-05-28 11:49:11 -0500

    added missing folders

--- silkie-chicken/app.js
+++ silkie-chicken/app.js
@@ -5,13 +5,35 @@
 //     console.log("I hope it worked!!")
 // }
 
-const button = document.querySelector('button');
-button.addEventListener('click', function(){
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    const newColor = `rgb(${r},${g},${b})`
-    document.body.style.backgroundColor = newColor;
+// random color code
+// const button = document.querySelector('button');
+// button.addEventListener('click', function(){
+//     const r = Math.floor(Math.random() * 255);
+//     const g = Math.floor(Math.random() * 255);
+//     const b = Math.floor(Math.random() * 255);
+//     const newColor = `rgb(${r},${g},${b})`
+//     document.body.style.backgroundColor = newColor;
+// });
 
-})
+// document.querySelector('button').addEventListener('click', function(){
+//     console.log(evt)
+// });
+// const input = document.querySelector('input');
+// input.addEventListener()
+
+// ADD TO DO LIST
+// const form = document.querySelector("#shelterForm");
+// const input = document.querySelector('#catName');
+// const list = document.querySelector("#cats");
+// form.addEventListener("submit", function (e){
+//     e.preventDefault()
+//    // console.log(input.value)
+//     const catName = input.value;
+//     const newLi = document.createElement("li")
+//     newLi.innerText = catName;
+//     list.append(newLi);
+// });
+
+
+
 
