a2e79541bb6d332d825ed195712ff2a89f60178e
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-04 07:53:48 -0500

    dom exercise completed

---
+++ dom-manipulation-exercises/app.js
@@ -0,0 +1,17 @@
+// adding elements to the page
+// const body = document.body
+// // body.append("Hello World", "Bye")
+// const div = document.createElement('div');
+// div.innerText = "This is inner Text"
+// div.textContent = "This is Text Content"
+// div.innerHTML = "<strong>I am strong</strong>"
+// body.append(div);
+// console.log(div.textContent);
+// console.log(div.innerText);
+
+const body = document.body
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+console.log(getAttribute('id'))
