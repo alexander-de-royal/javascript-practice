b3434460df754374da1bd8a1d4349c4bfbb8fb2c
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-04 10:16:08 -0500

    nodemon exercises completed

--- first-app/index.js
+++ first-app/index.js
@@ -17,6 +17,22 @@ app.get('/dogs', (req,res) =>{
     // console.log("Woof!")
     res.send('Woof')
 })
+app.get('r/:subreddit/', (req,res)=>{
+    const {subreddit} = req.params;
+    res.send(`<h1>Browsing the ${subreddit}</h1>`);
+})
+app.get('r/:subreddit/:postId', (req,res)=>{
+    const {subreddit} = req.params;
+    res.send(`<h1>Browsing the postId of ${subreddit}</h1>`);
+})
+app.get('/search', (req,res)=>{
+    console.log(req.query);
+    const {q} = req.query;
+    if(!q){
+        res.send('nothing found nothing searched')
+    }
+    res.send({q});
+})
 
 app.listen(3000, () =>{
     console.log("Server started on port 3000")
