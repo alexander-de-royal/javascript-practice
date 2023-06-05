17302c88379ba5cd177f094e23d608327287b465
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-05 14:51:37 -0500

    dom exercise completed

---
+++ ninja-dom/sandbox.js
@@ -0,0 +1,59 @@
+// const para = document.querySelector('p')
+// const para2 = document.querySelector('.p')
+// const para3 = document.querySelector('div.error')
+// const paras = document.querySelectorAll('p')
+// paras.forEach()
+// console.log(para)
+// console.log(para2)
+// console.log(para3)
+// console.log(paras)
+
+// const para = document.querySelector('p');
+// const paras = document.querySelectorAll('p');
+// para.innerText = "ninjas are awesome"
+// console.log(para.innerText);
+
+// paras.forEach(para=>{
+//     console.log(para.innerText)
+//     paras.innerText += 'New Text'
+// })
+
+// const content = document.querySelector('.content');
+// content.innerHTML = '<h2>This is a new h2</h2>'
+
+// const people = ['mario', 'luigi', 'yoshi'];
+// people.forEach(person => {
+//     content.innerHTML += `<p>${person}</p>`
+// })
+
+// const link = document.querySelector('a');
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.facebook.com');
+// link.innerText = 'facebook'
+//
+// const msg = document.querySelector('p');
+//
+// console.log(msg.getAttribute('class'));
+// msg.setAttribute('class', 'success');
+// msg.setAttribute('style', 'color:green')
+
+// const title = document.querySelector('h1')
+// // title.setAttribute('style', 'margin: 50px')
+// console.log(title.style);
+// this.style.margin = ''
+
+// const content = document.querySelector('p');
+// console.log(content.classList);
+// content.classList.add('error');
+// content.classList.remove('error');
+// content.classList.add('success');
+
+// const paras = document.querySelectorAll('p')
+// paras.forEach(p => {
+//     if(p.textContent.includes('error')){
+//         p.classList.add('error')
+//     }
+//     if(p.textContent.includes('success')){
+//         p.classList.add('success');
+//     }
+// })
