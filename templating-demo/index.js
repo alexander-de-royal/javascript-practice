f70c3716481a0cd3fe4c257bf75efa18c26f53ec
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-04 21:08:25 -0500

    practice on ejs

--- templating-demo/index.js
+++ templating-demo/index.js
@@ -7,16 +7,21 @@ app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res)=>{
     // res.send("./")
-    res.render('home.ejs')
+    res.render('home')
 })
-
+app.get('/cats', (req,res) =>{
+    const cats = [
+        'blue', 'rocket', 'monty', 'stephanie', 'winston'
+    ]
+    res.render('cats', {cats})
+})
 app.get('/r/:subreddit', (req, res)=>{
     const {subreddit} = req.params;
     res.render('subreddit', {subreddit});
 })
 app.get('/rand', (req,res)=>{
     const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', {rand: num})
+    res.render('random', {num})
 })
 app.listen(3000, ()=>{
     console.log("Server started on port 3000")
